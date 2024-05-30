625c13dec11b01891d832f6030afdea6fcff90dc
Abhay Charan das <abhay.charan.d@gmail.com>
2024-05-30 19:53:41 +0300

    refactored TestU01

--- TestU01/AKalashnikovaGaloisRightLFSR.c
+++ TestU01/AKalashnikovaGaloisRightLFSR.c
@@ -1,32 +1,20 @@
-#include <stdint.h>
+#include <testu01/unif01.h>
+#include <testu01/bbattery.h>
 #include <stdio.h>
 
-static uint64_t LFSR_GAMMA =   0x0af944215c1040f7ULL; // initialize LFSR register by Prime numbers
+unsigned int AKalashnikovaGaloisRightLFSR (void);
 
-unsigned int AKalashnikovaGaloisRightLFSR (void)
+int main (void)
 {
-//    printf("LFSR_GAMMA hex = 0x%lx\n", LFSR_GAMMA);
+   //printf("sizeof(unsigned int) = %ld\n", sizeof(unsigned int));
 
-    uint32_t res = (uint32_t)(LFSR_GAMMA>>32); // 0xFFFFFFFF00000000u
+   unif01_Gen *gen;
 
-    for (int j=0; j<32; j++ )
-    {
-        // taps:  64, 4, 3, 1, 0 ; feedback polynomial: G(x) = x^64 + x^4 + x^3 + x^1 + 1
-        uint64_t feedback_bit = LFSR_GAMMA & 1u;  // Get LSB (i.e., the output bit).
-        // 40:
-        LFSR_GAMMA = (LFSR_GAMMA >> 1) ^ (-feedback_bit & 0xD800000000000000u);
-        // 41:
-        /*
-        LFSR_GAMMA >>= 1;                                        // Shift register.
-        if (feedback_bit)                                        // If the output bit is 1,
-              LFSR_GAMMA ^= 0xD800000000000000u; //    apply toggle mask.
-              // 1101100000000000000000000000000000000000000000000000000000000000
-        */
-        // 42:
-        //LFSR_GAMMA >>= 1;                                        // Shift register.
-        //LFSR_GAMMA ^= (-feedback_bit) & 0xD800000000000000u;
+   gen = unif01_CreateExternGenBits ("AKalashnikovaGaloisRightFSR", AKalashnikovaGaloisRightLFSR);
+   bbattery_SmallCrush (gen);
+   //bbattery_Crush (gen);
 
-    }
+   unif01_DeleteExternGenBits (gen);
 
-    return res;
+   return 0;
 }
